2b65f78b5302913055c5bc7070a3d266197db1b3
lucaryholt <luca@ryholt.net>
2019-11-29 11:08:55 +0100

    initial commit

---
+++ src/main/java/com/firstyearproject/salontina/SalonTinaApplication.java
@@ -0,0 +1,13 @@
+package com.firstyearproject.salontina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalonTinaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SalonTinaApplication.class, args);
+    }
+
+}
