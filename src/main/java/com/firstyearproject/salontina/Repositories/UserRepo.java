84f44d787220aa543589eb4d9c59026d9c61e43d
lucaryholt <luca@ryholt.net>
2019-12-02 20:59:05 +0100

    added methods to repo interfaces

--- src/main/java/com/firstyearproject/salontina/Repositories/UserRepo.java
+++ src/main/java/com/firstyearproject/salontina/Repositories/UserRepo.java
@@ -1,4 +1,10 @@
 package com.firstyearproject.salontina.Repositories;
 
+import com.firstyearproject.salontina.Models.User;
+
+import java.util.List;
+
 public interface UserRepo {
+    List<String> getNewsletterList();
+    boolean addUser(User user);
 }
