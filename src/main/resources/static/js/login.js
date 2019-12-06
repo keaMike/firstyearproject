1acdf158a695aa2712d702e7c5ae50b9b4625b99
keaMike <m.eggertsen@hotmail.com>
2019-12-06 09:45:55 +0100

    js comment added

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -1,3 +1,4 @@
+//Selects all html lines with 'input' as class
 const inputs = document.querySelectorAll('.input');
 
 function focusFunc() {
@@ -12,15 +13,19 @@ function blurFunc() {
     }
 }
 
+//For each 'input' add event listener, if clicked add 'focus' to class, which triggers CSS, that performs the animation.
+//if clicked outside input, add 'blur', which removes 'focus' from class and relation to CSS.
 inputs.forEach(input => {
     input.addEventListener('focus', focusFunc);
     input.addEventListener('blur', blurFunc);
 });
 
 //CONFIRM PASSWORD
+//Select element with id = 'password' and 'confirmpassword' and save them as an variable
 var password = document.getElementById("password")
     , confirm_password = document.getElementById("confirmpassword");
 
+//Checks if both passwords are identical
 function validatePassword(){
     if(password.value != confirm_password.value) {
         confirm_password.setCustomValidity("Kodeordene er ikke ens");
@@ -29,5 +34,6 @@ function validatePassword(){
     }
 }
 
+//When password changes and confirm password letter is pressed, check if identical
 password.onchange = validatePassword;
 confirm_password.onkeyup = validatePassword;
